f04b31b70d60c5632f4c52a7ac34b717caa8f915
letusgitcommit <davidvaughn006@gmail.com>
2023-01-16 21:03:57 -0600

    Added in pip_lock to make sure that dependencies are kept up to date across development environments

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import os
 import sys
+from pathlib import Path
 
 
 def main():
@@ -17,6 +18,21 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    try:
+        import pip_lock
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError(
+            "Couldn't import pip_lock. Are you on the right env "
+            + "and are your dependencies up to date?"
+        )
+
+    requirements_path = str(Path(__file__).parent / "requirements.txt")
+    pip_lock.check_requirements(
+        requirements_path,
+        post_text="\nRun the following:\n\npython -m pip install -r requirements.txt",
+    )
+
     execute_from_command_line(sys.argv)
 
 
